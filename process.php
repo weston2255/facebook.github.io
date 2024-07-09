e10c51f82ec601aa048cb237a846e5517a3ef3c5
weston2255 <cedricnyobo@gmail.com>
2024-07-09 05:26:16 +0000

    Update process.php

--- process.php
+++ process.php
@@ -1,16 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $phone = $_POST['phone'];
-    $old_password = $_POST['old-password'];
-    $new_password = $_POST['new-password'];
 
-    // Affiche les informations recueillies (pour une démonstration uniquement)
-    echo "Nom d'utilisateur : " . htmlspecialchars($username) . "<br>";
-    echo "Numéro de téléphone : " . htmlspecialchars($phone) . "<br>";
-    echo "Ancien mot de passe : " . htmlspecialchars($old_password) . "<br>";
-    echo "Nouveau mot de passe : " . htmlspecialchars($new_password) . "<br>";
-
-    // Vous pouvez également envoyer ces informations à un email ou les stocker dans une base de données
-}
-?>
